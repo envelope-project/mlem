a5ebdc5c1a4d397be3c52313b8f29ae5eb85c4ad
Dai Yang <d.yang@tum.de>
2017-08-08 08:12:12 +0200

    Added Copyright Infomration Header

--- csrgen/fisher.cc
+++ csrgen/fisher.cc
@@ -1,21 +1,20 @@
 /**
-  csr4gen
+    Copyright © 2017 Thorsten Fuchs
+    Copyright © 2017 LRR, TU Muenchen
+    Authors: Thorsten Fuchs
 
-  Copyright © 2017 Thorsten Fuchs
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+    OTHER DEALINGS IN THE SOFTWARE.
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-/**
- * @file fisher.cc
- * @author Thorsten Fuchs
- * @date 18 Jun 2017
- * @brief Provide Fisher-Yates Shuffle with additional sorting capability 
- *        of the first n elements
+    The above copyright notice and this permission notice shall be 
+    included in all copies and/or substantial portions of the Software,
+    including binaries.
 */
 
 #include <random>
