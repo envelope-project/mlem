8fd758afbfa6a37141de08ab932a2e792813e366
Dai <me@daiyang.de>
2018-10-09 11:21:32 +0200

    Added Copyright Notifications

--- src/mpicsr4mlem.cpp
+++ src/mpicsr4mlem.cpp
@@ -1,3 +1,23 @@
+/**
+    Copyright © 2017 Technische Universitaet Muenchen
+    Authors: Tilman Kuestner
+           Dai Yang
+           Josef Weidendorfer
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+    OTHER DEALINGS IN THE SOFTWARE.
+
+    The above copyright notice and this permission notice shall be 
+    included in all copies and/or substantial portions of the Software,
+    including binaries.
+*/
+
 #define _LARGEFILE64_SOURCE
 #ifndef __APPLE__
 #include <xmmintrin.h>
--- src/vector.hpp
+++ src/vector.hpp
@@ -1,11 +1,22 @@
 /**
- * vector.hpp
- *
- * Created on: Jun 6 2013
- *     Author: kuestner
- *
- * A simple fixed length vector/array
- */
+    Copyright © 2017 Technische Universitaet Muenchen
+    Authors: Tilman Kuestner
+           Dai Yang
+           Josef Weidendorfer
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+    OTHER DEALINGS IN THE SOFTWARE.
+
+    The above copyright notice and this permission notice shall be 
+    included in all copies and/or substantial portions of the Software,
+    including binaries.
+*/
 
 #ifndef _VECTOR_H_
 #define _VECTOR_H_
