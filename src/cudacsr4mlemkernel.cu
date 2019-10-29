05c11ea44b131dcfbfdf0ea01f09a854b69bc2b0
Dai Yang <d.yang@tum.de>
2019-10-29 16:01:34 +0100

    copyright fix

--- src/cudacsr4mlemkernel.cu
+++ src/cudacsr4mlemkernel.cu
@@ -1,3 +1,23 @@
+/**
+    Copyright © 2019 Technische Universitaet Muenchen
+    Authors: Mengdi Wang
+           Apporva Gupta
+           Dai Yang
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+    OTHER DEALINGS IN THE SOFTWARE.
+
+    The above copyright notice and this permission notice shall be 
+    included in all copies and/or substantial portions of the Software,
+    including binaries.
+*/
+
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include "../include/cudacsr4mlem.cuh"
